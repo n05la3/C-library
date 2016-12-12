6e90ea61fd2d71fe4dff4d532035be97310919fc
Calson Noah <chiacalson@gmail.com>
2016-12-13 00:37:54 +0100

    More work on poointers and arrays

--- sumarray.c
+++ sumarray.c
@@ -1,9 +1,21 @@
 //sumarray.c: summing arrays using pointer arithmetic
 #include<stdio.h>
 #define SIZE 10
+float sum_array(float* number_pointer, float* end);
 int main(void)
 {
-	const int my_array[SIZE] 
-
-
+	float my_array[SIZE] = {4,2,3,4,5,6,7,8,9,10};
+	printf("%s: %3.3f\n","The sum of the ten numbers is",sum_array(my_array,(my_array+SIZE)));
 }
+float sum_array(float* number_pointer, float* end)
+{
+	float total = 0;
+	while(number_pointer<end)
+	{	
+		total += *(number_pointer++);
+        
+	}
+	return total;
+    
+} 
+
