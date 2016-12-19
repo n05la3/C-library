09e97498c801daa77a0a79ec40f0833bfeb1bde9
Calson Noah <chiacalson@gmail.com>
2016-12-19 23:12:05 +0100

    change values of array to test the function

--- arrayindex.c
+++ arrayindex.c
@@ -1,21 +1,22 @@
 //larger_in_array: Function to return the larger element in an array of int
 #include<stdio.h>
 #include<stdlib.h>
-#define SIZE 5
+#define SIZE 6
 int larger_int(const int *num, int n)
 {
-     int i,max=*num;
+     int i;
+     const int *max=num;
     for(i=0;i<n;i++)
     {
-    	if(*(num+i)>max)
-    		max = *(num+i);
+    	if(*(num+i)>*max)
+    		max = (num+i);
     }
-    return (num+i)-num;
+    return (max-num);
 }
 int main(void)
 {
-	const int array_to_compare[SIZE]={2,45,6,7,20};
+	const int array_to_compare[SIZE]={2,7,50,7,20,9};
     printf("%s %d %s %d \n",
-    	"The larger of the five elements is: ",array_to_compare[larger_int(array_to_compare,SIZE)-1],"and it's index is: ",larger_int(array_to_compare,SIZE));
+    	"The larger of the five elements is: ",array_to_compare[larger_int(array_to_compare,SIZE)],"and it's index is: ",larger_int(array_to_compare,SIZE));
 	exit(0);
 }
