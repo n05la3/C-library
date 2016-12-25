0bf8dffac399a4ce50ae1c60bce18114a1c166ae
Calson Noah <chiacalson@gmail.com>
2016-12-25 17:13:23 +0100

    Using the strcmp function

--- namesearch.c
+++ namesearch.c
@@ -1,8 +1,15 @@
 //namesearch.c: Learning to use the strncmp function
 #include<stdio.h>
-
+#include<string.h>
+const char name[81]= "Prince";
 
 int main(void)
 {
-
+	if(strcmp(name,"Princee")==0)
+	{
+		printf("Yes Calson is found in the string given!\n");
+	}
+    else 
+    	printf("%s\n","The entered name is not found in the string\n");
+    return 0;
 }
