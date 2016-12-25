d0e942199dd4e7c93896ade636126aa762b69631
Calson Noah <chiacalson@gmail.com>
2016-12-25 16:38:45 +0100

    Added more exercises

--- stringelementaccess.c
+++ stringelementaccess.c
@@ -1,1 +1,9 @@
 //stringelementaccess.c: understanding the relation between strings and characters
+#include<stdio.h>
+int main(void)
+{
+	const char name[81] = "my name is D Pro";
+	printf("%c\n",*(name+16));
+	return 0;
+
+}
