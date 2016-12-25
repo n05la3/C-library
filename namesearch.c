bf43e30b83e298bdda67e1481bd2c2bfaef7eefc
Calson Noah <chiacalson@gmail.com>
2016-12-26 00:50:57 +0100

    corrected logic error

--- namesearch.c
+++ namesearch.c
@@ -2,18 +2,33 @@
 #include<stdio.h>
 #include<string.h>
 #define SIZE 10
-const char *name[SIZE]= {"Prince, Pot, Pen, Pineaple, People, Pin, Purse, Program, Pet, Pen"};
+//const char *my_p_name[SIZE]= {"Prince, Pot, Pen, Pineaple, People, Pin, Purse, Program, Pet, Pen"};
+int p_search(char *p_guess, char *p_name,char *p_found,int n)
+{
+	int i,p_count=0;
+	for(i=0; i<n;i++)
+		if(!strcmp(p_guess,p_name[i]))
+    	{
+    		p_found+i=name[i];
+    		p_count++;
+    	}
+    return p_count;
+}
+
+void printstr(char *array)
+{
+	int i;
+	for(i=0;i<SIZE;i++)
+		printf("%s", array);
+}
 
 int main(void)
 {
-	char my_guess[81];
-	int i;
+	char my_guess[81],correct_guess[81];
+	
 	printf("Guess a name starting with P, stored in the p-name database: ");
     fgets(my_guess,81,stdin);
-    for(i=0; i<SIZE,i++)
-    {
-    	if(!strcmp(my_guess,name[i]))
-    		return 
-    }
+    p_search(my_guess,my_p_name,correct_guess,SIZE);
+    printstr(correct_guess);
     return 0;
 }
