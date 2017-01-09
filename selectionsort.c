85762254f819b777e081ea6bf14262c0cb1e05ea
Calson Noah <chiacalson@gmail.com>
2017-01-09 01:10:59 +0100

    debugging

--- selectionsort.c
+++ selectionsort.c
@@ -1,24 +1,31 @@
 //selectionsort.c: Using the selection sort algorithm to sort int elements in an array
 #include<stdio.h>
 #define SIZE 10
-void swap(int *a,int *b)
+void swap(int *a,int *b,int n)//Only used because function won't require input from user.
 {
-	*a=*a**b;
-	*b=*a/(*b);
-	*a=*a/(*b);
+	*a=*a*(*b+n);
+	*(b+n)=*a/(*b+n);
+	*a=*a/(*b+n);
+	//or use the addition and subtraction concepts
 }
-
+char ch;
 void select_sortf(int *arr_to_sort)
 {
 	int i,j,min=*arr_to_sort;
-	for(j=0;j<SIZE-1;++j)
+	printf("%d",arr_to_sort[0]);
+	ch=getchar();
+	for(j=0;j<SIZE;++j)
 	{
-		for(i=j;i<SIZE;++i)
+		for(i=j;i<SIZE-1;i++)
+		{
 			if(*(arr_to_sort+i)<(*arr_to_sort+i+1))
 				min=*(arr_to_sort+i);
 			else min=*(arr_to_sort+i+1);
-		swap(&min,arr_to_sort);
-	    arr_to_sort[i+1]=min;
+		}
+		printf(": %d",min);
+		ch=getchar();
+		//swap(&min,arr_to_sort,j);
+	    //arr_to_sort[j]=min;
 	}
 }
 
@@ -27,7 +34,7 @@ void print_arr(int *arr)
 	int i;
 	for(i=0;i<SIZE;++i)
 		printf("%d,",*(arr+i));
-	printf("\b\b");
+	printf("\b.\n");
 }
 
 int main(void)
@@ -36,7 +43,7 @@ int main(void)
 	printf("Unsorted array: ");
 	print_arr(ints_to_sort);
 	select_sortf(ints_to_sort);
-	printf("\nSelection sorted arrays: ");
+	printf("\nSelection sorted array: ");
 	print_arr(ints_to_sort);
 	return 0;
 }
