bf3326633c3ffb9847e813acb3cc540094722e96
Calson Noah <chiacalson@gmail.com>
2017-01-13 16:22:03 +0100

    using a different approach to sort ints using the selection sort algorithm

--- selectionsort.c
+++ selectionsort.c
@@ -1,31 +1,25 @@
 //selectionsort.c: Using the selection sort algorithm to sort int elements in an array
 #include<stdio.h>
 #define SIZE 10
-void swap(int *a,int *b,int n)//Only used because function won't require input from user.
+void swap(int *ints_to_sort,int a,int b)//Only used because function won't require input from user.
 {
-	*a=*a*(*b+n);
-	*(b+n)=*a/(*b+n);
-	*a=*a/(*b+n);
+	*(ints_to_sort+a)=*(ints_to_sort+a)**(ints_to_sort+b);
+	*(ints_to_sort+b)=*(ints_to_sort+a)/(*(ints_to_sort+b));
+	*(ints_to_sort+a)=*(ints_to_sort+a)/(*(ints_to_sort+b));
 	//or use the addition and subtraction concepts
 }
 char ch;
 void select_sortf(int *arr_to_sort)
 {
-	int i,j,min=*arr_to_sort;
-	printf("%d",arr_to_sort[0]);
-	ch=getchar();
-	for(j=0;j<SIZE;++j)
+	int i,j,i_min;//i_min: int variable to hold the index for the smallest number in the array.
+	for(j=0;j<SIZE;j++)
 	{
-		for(i=j;i<SIZE-1;i++)
-		{
-			if(*(arr_to_sort+i)<(*arr_to_sort+i+1))
-				min=*(arr_to_sort+i);
-			else min=*(arr_to_sort+i+1);
-		}
-		printf(": %d",min);
-		ch=getchar();
-		//swap(&min,arr_to_sort,j);
-	    //arr_to_sort[j]=min;
+		i_min=j;
+		for(i=j+1;i<SIZE;i++)
+			if(*(arr_to_sort+i)<*(arr_to_sort+i_min))
+				i_min=i;
+		if(i_min!=j)
+			swap(arr_to_sort,j,i_min);
 	}
 }
 
@@ -39,7 +33,7 @@ void print_arr(int *arr)
 
 int main(void)
 {
-	int ints_to_sort[SIZE]={10,9,8,7,6,5,4,3,2,1};
+	int ints_to_sort[SIZE]={1,5,4,4,5,6,7,8,-9,10};//test data should not include a zero else swap fails.
 	printf("Unsorted array: ");
 	print_arr(ints_to_sort);
 	select_sortf(ints_to_sort);
