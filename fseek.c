60703e5e22fcddccf886d4e5f326a7fc6c381fc4
Calson Noah <chiacalson@gmail.com>
2017-02-02 19:55:32 +0100

    testing the implementation

--- fseek.c
+++ fseek.c
@@ -7,22 +7,26 @@
  {
  	FILE *fp;
  	long LEN;
- 	char name[20];
- 	if((fp=fopen("userpass","w+"))==NULL)
+ 	char name[81];
+ 	if((fp=fopen("userpass","r"))==NULL)
  	{
  		fprintf(stderr, "%s\n", "Unable to open file \"userpass\"");
  		exit(1);
  	}
- 	fprintf(fp, "%s\n","User name:" );
+ 	//fprintf(fp, "%s\n","Calson:" );
+
  	//rewind(fp);
  	//fseek(fp,11l,SEEK_SET);
- 	printf("Enter User name to store in file userpass: ");
- 	fgets(name,20,stdin);
- 	fget_ignore_newline(name);
- 	fprintf(fp,"%s\n",name);
+ 	//printf("Enter User name to store in file userpass: ");
+ 	//fgets(name,20,stdin);
+ 	//fget_ignore_newline(name);
+ 	//fprintf(fp,"%s\n",name);
  	//printf("lenght of name: %d",strlen(name));
  	//fseek(fp,18L,SEEK_SET);
- 	fprintf(fp, "%s\n","Password: ");
+ 	//fprintf(fp, "%s\n","Password: ");
+ 	rewind(fp);
+ 	fgets(name,81,fp);
+ 	printf("The name is: %s", name);
  	if(fclose(fp)!=0)
  		fprintf(stderr, "%s\n", "Unable to close opened file");
 
