7db2b501820104d193deef454b70e944144be4b0
Calson Noah <chiacalson@gmail.com>
2017-04-06 22:01:17 +0100

    Modified the program into a function

--- time.c
+++ time.c
@@ -4,17 +4,20 @@
 #include<stdlib.h>
 main()
 {
-	long sec, hr, min, t;
-	printf("Enter time in seconds: ");
-	scanf("%ld", &sec);
-	hr = sec/3600;
-	t = sec%3600;
-	min = t/60;
-	sec = t%60;
-	if(hr>0)
-		printf("%ld%s:%ld%s:%ld%s", hr,(hr>1)? "hrs":"hr",min,(min>1)? "mins":"min",sec,(sec>1)? "secs":"sec");//modify printing display
-	else if(min>0)
-		printf("%ld%s:%ld%s", min,(min>1)? "mins":"min",sec,(sec>1)? "secs":"sec");
- 	else 
-		printf("%ld%s", sec,(sec>1)? "secs":"sec");
+	void second_to_hms(long int time_in_seconds)
+	{
+			long hr, min, t;
+			hr = time_in_seconds/3600;
+			t = time_in_seconds%3600;
+			min = t/60;
+			time_in_seconds = t%60;
+			if(hr>0)
+				printf("%ld%s:%ld%s:%ld%s", hr,(hr>1)? "hrs":"hr",min,(min>1)? "mins":"min",time_in_seconds,(time_in_seconds>1)? "secs":"sec");//modify printing display
+			else if(min>0)
+				printf("%ld%s:%ld%s", min,(min>1)? "mins":"min",time_in_seconds,(time_in_seconds>1)? "secs":"sec");
+		 	else 
+				printf("%ld%s", time_in_seconds,(time_in_seconds>1)? "secs":"sec");
+	}
+	second_to_hms(90);
+
 }
